9092ca56ed7ffae81b540bb374ebb5c91d6ab898
Saif21212 <sai16fos@gmail.com>
2026-03-24 00:04:53 -0700

    Refactor LoggingAlertHandlerDecorator implementation

--- week4-exercises/mini09-decorator-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini09/LoggingAlertHandlerDecorator.java
+++ week4-exercises/mini09-decorator-pattern/src/main/java/edu/norcocollege/cis18b/weekx/mini09/LoggingAlertHandlerDecorator.java
@@ -1,16 +1,17 @@
 package edu.norcocollege.cis18b.weekx.mini09;
 
 public class LoggingAlertHandlerDecorator implements AlertHandler {
-    private final AlertHandler wrapped;
 
-    public LoggingAlertHandlerDecorator(AlertHandler wrapped) {
-        this.wrapped = wrapped;
+    private AlertHandler handler;
+
+    public LoggingAlertHandlerDecorator(AlertHandler handler) {
+        this.handler = handler;
     }
 
     @Override
     public void handle(Alert alert) {
-        // TODO: Print a message before delegating.
-        // TODO: Call the wrapped handler.
-        // TODO: Print a message after delegating.
+        System.out.println("LOG: Alert received");
+        handler.handle(alert);
+        System.out.println("LOG: Alert processed");
     }
 }
